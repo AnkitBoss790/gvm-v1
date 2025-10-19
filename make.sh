502fe9a59ea54851ccc90069b7b3a97878e429f3
Gamerzhacker😎 <bestservergamer@gmail.com>
2025-10-19 22:40:14 +0530

    Add files via upload

---
+++ make.sh
@@ -0,0 +1,40 @@
+# PASTE ALL
+echo "
+=============================================================
+
+                   PowerDev
+                  Maded By PowerDev 
+             Copyright (C) By PowerDev 
+             
+
+               VM V1 By Gamerzhacker 
+============================================================
+"
+
+git clone https://github.com/AnkitBoss790/gvm-v1.git
+apt install unzip -y && unzip v1-gvm.zip
+cd hvm
+apt update
+apt install python3 -y
+apt install neofetch -y && neofetch
+
+apt install python3-pip  
+python3 -m pip install --upgrade pip
+echo "
+=========================================================
+
+                    GVM STARTING IN 5 Secoands
+                      Admin:
+                     Username: admin
+                     Password: admin
+
+                    Check Port (" 4000 ")
+
+                      Copyright (C) By PowerDev
+==========================================================
+
+"
+
+pip install flask flask-socketio flask-login docker paramiko psutil python-dotenv email-validator flask_limiter
+# FINAL
+python3 gvm.py
