2f841322e453521efaa7fbbcf23ec3f1038c47d8
Gamerzhacker😎 <bestservergamer@gmail.com>
2025-10-19 23:16:06 +0530

    Update make.sh

--- make.sh
+++ make.sh
@@ -13,7 +13,7 @@ echo "
 
 git clone https://github.com/AnkitBoss790/gvm-v1.git
 apt install unzip -y && unzip v1-gvm.zip
-cd hvm
+cd gvm-v1
 apt update
 apt install python3 -y
 apt install neofetch -y && neofetch
